001da4762a9f0b522b8c34d885bce29008d5e5e6
KenInoue1911 <inouetkrk3@gamail.com>
2021-07-14 21:26:33 +0900

    lesson2復習2

---
+++ lesson2復習/while文.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int number = 10;
+
+    while (0 < number) {
+      Sytem.ou.pritnln(number);
+      number--;
+    }
+  }
+}
---
+++ lesson2復習/配列.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    System.out.println(names[0]);
+    System.out.println(names[2]);
+  }
+}

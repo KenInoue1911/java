d1e6c2b4b2561d5a643729887eada3420f70cb12
KenInoue1911 <inouetkrk3@gamail.com>
2021-07-17 19:42:14 +0900

    lesson3-1

---
+++ Lesson3/引数の利用.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(Stirng[] args) {
+    printData("Kate Jones");
+    printData("John chris");
+  }
+
+  public static void printData(String name) {
+    System.out.println("私の名前は"+name+"です")
+  }
+}
